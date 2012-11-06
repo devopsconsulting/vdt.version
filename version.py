080eff2ddd94a1d291f32a9b9c3305b292e3edf4
Cosmin Luță <cosmin.luta@avira.com>
2012-11-06 16:35:23 +0200

    Major changes

--- version.py
+++ version.py
@@ -1,98 +1,151 @@
 # -*- coding: utf-8 -*-
-# Author: Douglas Creager <dcreager@dcreager.net>
-# This file is placed into the public domain.
 
-# Calculates the current version number. If possible, this is the
-# output of “git describe”, modified to conform to the versioning
-# scheme that setuptools uses. If “git describe” returns an error
-# (most likely because we're in an unpacked copy of a release tarball,
-# rather than in a git working copy), then we fall back on reading the
-# contents of the RELEASE-VERSION file.
-#
-# To use this script, simply import it your setup.py file, and use the
-# results of get_git_version() as your package version:
-#
-# from version import *
-#
-# setup(
-# version=get_git_version(),
-# .
-# .
-# .
-# )
-#
-# This will automatically update the RELEASE-VERSION file, if
-# necessary. Note that the RELEASE-VERSION file should *not* be
-# checked into git; please add it to your top-level .gitignore file.
-#
-# You'll probably want to distribute the RELEASE-VERSION file in your
-# sdist tarballs; to do this, just create a MANIFEST.in file that
-# contains the following line:
-#
-# include RELEASE-VERSION
+import logging
+import subprocess
+import argparse
+import os
 
-__all__ = ("get_git_version")
+BUILD_TAG = "-jenkins-"
 
-from subprocess import Popen, PIPE
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('repo-ver')
 
-
-def call_git_describe(abbrev=4):
+def get_build_tags_from_version(base_version, build_tag=BUILD_TAG):
+    """
+    Return all tags that look like "base_version-BUILD_TAG-buildnumber"
+    """
     try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
-                  stdout=PIPE, stderr=PIPE)
-        p.stderr.close()
-        line = p.stdout.readlines()[0]
-        return line.strip()
+        args = ['git', 'tag', '-l', "{0}{1}*".format(base_version, build_tag)]
+        output = subprocess.check_output(args, stderr=None).strip().split("\n")
+        return [line for line in output if line]
+    except Exception as e:
+        log.exception(e)
 
-    except Exception, e:
-        print e
-        return None
+def create_new_build_tag(ver, build_number, build_tag=BUILD_TAG):
+    """
+    Create a new tag in the form of "base_version-build_tag-build_number"
+    """
+    try:
+        version = ".".join(map(str, ver))
+        args = ["git", "tag", "{0}{1}{2}".format(version, build_tag, build_number)]
+        subprocess.check_call(args)
+    except Exception as e:
+        log.exception(e)
+
+def create_new_version_tag(ver, changelog, build_tag=BUILD_TAG):
+    """
+    Create a new tag in the form of "base_version-build_tag-build_number"
+    """
+    try:
+        version = ".".join(map(str, ver))
+        args = ["git", "tag", "-a", version, "-m", changelog]
+        subprocess.check_call(args)
+    except Exception as e:
+        log.exception(e)
+
+def _get_latest_build_number(tags):
+    """
+    Return the latest (highest) build number out of tags. It's assumed that all
+    tags are of the same version, so that's not checked anymore.
+    """
+    if not tags:
+        return 0
+
+    def _build_number(tag):
+        try:
+            return int(tag.split("-")[2])
+        except:
+            log.error("error fetching build number from {0}".format(tag))
+            return 0
+    return max(_build_number(tag) for tag in tags)
 
 
-def get_git_version(abbrev=4):
-    # Read in the version that's currently in RELEASE-VERSION.
-
-    release_version = read_release_version()
-
+def get_git_version():
     # First try to get the current version using “git describe”.
+    try:
+        args = ['git', 'describe', '--abbrev=4']
+        line = subprocess.check_output(args, stderr=None)
+        version = line.strip()
+    except Exception as e:
+        log.error(e)
+        raise StandardError("version not found")
 
-    version = call_git_describe(abbrev)
+    if not version:
+        raise StandardError("Cannot find the version number!")
 
-    #adapt to PEP 386 compatible versioning scheme
-    version = pep386adapt(version)
-
-    # If that doesn't work, fall back on the value that's in
-    # RELEASE-VERSION.
-
-    if version is None:
-        version = release_version
-
-    # If we still don't have anything, that's an error.
-
-    if version is None:
-        raise ValueError("Cannot find the version number!")
-
-    # If the current version is different from what's in the
-    # RELEASE-VERSION file, update the file to be current.
-
-    if version != release_version:
-        write_release_version(version)
-
-    # Finally, return the current version.
-
-    return version
-
-
-def pep386adapt(version):
-    if version is not None and '-' in version:
-        # adapt git-describe version to be in line with PEP 386
-        parts = version.split('-')
-        parts[-2] = 'post'+parts[-2]
-        version = '.'.join(parts[:-1])
-        return version
+    if '-' in version:
+        version_number, _ = version.split("-", 1)
+        return version_number
     else:
         return version
 
 
+def main():
+    p = argparse.ArgumentParser(description="Version increment tool for GIT repositories")
+
+    p.add_argument("-p", "--patch", dest="patch", default=False, action="store_true", help="increment the patch number")
+    p.add_argument("-m", "--minor", dest="minor", default=False, action="store_true", help="increment minor number")
+    p.add_argument("-M", "--major", dest="major", default=False, action="store_true", help="increment major number")
+    p.add_argument("-b", "--build", dest="build", default=False, action="store_true", help="increment build number")
+    p.add_argument("-c", "--changelog", dest="changelog", help="description of the changes in the new version")
+
+    p.add_argument("-n", "--dry-run", dest="dry_run", default=False, help="don't perform any changes")
+
+    args = p.parse_args()
+
+    try:
+        ver_str = get_git_version()
+        ver = map(int, ver_str.split(".", 3))
+        log.debug("latest version is {0}".format(ver))
+    except StandardError:
+        log.error("cannot find the current version. Please create an adnotated tag x.y.z!")
+        return -1
+
+    try:
+        tags = get_build_tags_from_version(ver_str)
+        log.debug("build tags for version {0}: {1}".format(ver_str, tags))
+    except:
+        log.error("cannot get tags")
+        return -2
+
+    last_build_number = _get_latest_build_number(tags)
+    log.debug("latest build number for version {0}: {1}".format(ver_str, last_build_number))
+
+    # See what operation needs to be performed
+    ver_changed = False
+    if args.patch:
+        ver[2] += 1
+        ver_changed = True
+
+    if args.minor:
+        ver[1] += 1
+        ver[2] = 0
+        ver_changed = True
+
+    if args.major:
+        ver[0] += 1
+        ver[1] = 0
+        ver[2] = 0
+        ver_changed = True
+
+    # if we need to do a version increment, see that we have a changelog
+    if ver_changed:
+        if not args.changelog:
+            log.error("you need to specify a changelog when incrementing a version")
+            return -3
+        if os.path.isfile(args.changelog):
+            with open(args.changelog) as f:
+                changelog = f.read()
+        else:
+            changelog = args.changelog
+
+        create_new_version_tag(ver, changelog)
+
+    if args.build:
+        last_build_number += 1
+        log.debug("incremented build number: {0}".format(last_build_number))
+        create_new_build_tag(ver, last_build_number)
+
 if __name__ == "__main__":
-    print get_git_version()
+    main()
+
