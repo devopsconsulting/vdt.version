d0070e97917daa8cf9cdd20264cbb886b260a094
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-05-01 10:23:42 +0200

    Fixes git branch not reset correctly. Fixes silencing error return codes. refs devopsconsulting/vdt.version#1

--- vdt/version/shared.py
+++ vdt/version/shared.py
@@ -129,9 +129,12 @@ class Version(object):
             branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).rstrip()
             subprocess.check_call(['git', 'checkout', str(self)])
             yield
-            subprocess.check_call(['git', 'checkout', branch])
         except subprocess.CalledProcessError as e:
             log.error("Package creation failed: {0}".format(e))
+            log.error(e.output)
+            sys.exit(e.returncode)
+        finally:
+            subprocess.check_call(['git', 'checkout', branch])
 
     def __str__(self):
         if self.build_number is not None:
