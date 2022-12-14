b08157f6f89a4728a7557b904d8f161a70e349d2
Lars van de Kerkhof <lars@permanentmarkers.nl>
2022-12-14 13:04:44 +0100

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 pkgname = "vdt.version"
 
 setup(name=pkgname,
-      version="0.1.6",
+      version="0.1.7",
       description="Version Increment Tool for GIT",
       author="Lars van de Kerkhof",
       author_email="lars@devopsconsulting.nl",
